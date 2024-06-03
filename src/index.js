1ad334f9c415901e442bdd7ea7b6bca9f3763704
Bárbara Rolón <barbara.rolon.rivero@gmail.com>
2024-06-02 21:33:58 -0500

    cambiar queryselector

--- src/index.js
+++ src/index.js
@@ -6,36 +6,36 @@ const button = document.getElementById("reset-button");
 
 textarea.addEventListener("input", function (event) {
   const wordCount = analyzer.getWordCount(event.target.value);
-  document.querySelector(
-    ".one"
-  ).textContent = `Recuento de palabras: ${wordCount}`;
+  document.querySelectorAll(
+    ".item"
+  )[0].textContent = `Recuento de palabras: ${wordCount}`;
 
   const charCount = analyzer.getCharacterCount(event.target.value);
-  document.querySelector(
-    ".two"
-  ).textContent = `Recuento de caracteres: ${charCount}`;
+  document.querySelectorAll(
+    ".item"
+  )[1].textContent = `Recuento de caracteres: ${charCount}`;
 
   const charCountNoSpaces = analyzer.getCharacterCountExcludingSpaces(
     event.target.value
   );
-  document.querySelector(
-    ".three"
-  ).textContent = `Recuento de caracteres excluyendo espacios y signos de puntuacion: ${charCountNoSpaces}`;
+  document.querySelectorAll(
+    ".item"
+  )[2].textContent = `Recuento de caracteres excluyendo espacios y signos de puntuacion: ${charCountNoSpaces}`;
 
   const numberCount = analyzer.getNumberCount(event.target.value);
-  document.querySelector(
-    ".four"
-  ).textContent = `Recuento de numeros: ${numberCount}`;
+  document.querySelectorAll(
+    ".item"
+  )[3].textContent = `Recuento de numeros: ${numberCount}`;
 
   const numberSum = analyzer.getNumberSum(event.target.value);
-  document.querySelector(
-    ".five"
-  ).textContent = `Suma total de numeros: ${numberSum}`;
+  document.querySelectorAll(
+    ".item"
+  )[4].textContent = `Suma total de numeros: ${numberSum}`;
 
   const averageWordLength = analyzer.getAverageWordLength(event.target.value);
-  document.querySelector(
-    ".six"
-  ).textContent = `Longitud media de las palabras: ${averageWordLength.toFixed(
+  document.querySelectorAll(
+    ".item"
+  )[5].textContent = `Longitud media de las palabras: ${averageWordLength.toFixed(
     2
   )}`;
 });
