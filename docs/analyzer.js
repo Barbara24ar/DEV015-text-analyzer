7f843934a93ed1fba16a90dbffea0d2e7178cd94
Bárbara Rolón <barbara.rolon.rivero@gmail.com>
2024-06-02 21:56:26 -0500

    agregar carpeta docs

---
+++ docs/analyzer.js
@@ -0,0 +1,62 @@
+const analyzer = {
+  getWordCount: (text) => {
+    text = text.trim();
+    if (text === "") {
+      return 0;
+    }
+    const words = text.split(/\s+/);
+    return words.length;
+  },
+  getCharacterCount: (text) => {
+    const numero_de_caracteres = text.length;
+    return numero_de_caracteres;
+  },
+  getCharacterCountExcludingSpaces: (text) => {
+    if (typeof text !== "string") {
+      return 0;
+    }
+    let count = 0;
+    for (let i = 0; i < text.length; i++) {
+      const char = text[i];
+      if (char.match(/[\w]/)) {
+        count++;
+      }
+    }
+    return count;
+  },
+  getAverageWordLength: (text) => {
+    if (typeof text !== "string") {
+      return 0;
+    }
+    text = text.trim();
+    if (text === "") {
+      return 0;
+    }
+    const words = text.split(/\s+/);
+    if (words.length === 0) {
+      return 0;
+    }
+    let totalCharacters = 0;
+    for (const word of words) {
+      totalCharacters += word.length;
+    }
+    return parseFloat((totalCharacters / words.length).toFixed(2));
+  },
+  getNumberCount: (text) => {
+    const numbers = text.match(/\b-?\d+(\.\d+)?\b/g);
+    if (numbers === null) {
+      return 0;
+    }
+    return numbers.length;
+  },
+  getNumberSum: (text) => {
+    const numbers = text.match(/\b-?\d+(\.\d+)?\b/g);
+    if (numbers === null) {
+      return 0;
+    }
+    const sum = numbers.reduce((acc, num) => acc + parseFloat(num), 0);
+    return sum;
+  },
+};
+
+export default analyzer;
---
+++ docs/index.js
@@ -0,0 +1,45 @@
+import analyzer from "./analyzer.js";
+
+const textarea = document.querySelector("[name=user-input]");
+
+const button = document.getElementById("reset-button");
+
+textarea.addEventListener("input", function (event) {
+  const wordCount = analyzer.getWordCount(event.target.value);
+  document.querySelectorAll(
+    ".item"
+  )[0].textContent = `Recuento de palabras: ${wordCount}`;
+
+  const charCount = analyzer.getCharacterCount(event.target.value);
+  document.querySelectorAll(
+    ".item"
+  )[1].textContent = `Recuento de caracteres: ${charCount}`;
+
+  const charCountNoSpaces = analyzer.getCharacterCountExcludingSpaces(
+    event.target.value
+  );
+  document.querySelectorAll(
+    ".item"
+  )[2].textContent = `Recuento de caracteres excluyendo espacios y signos de puntuacion: ${charCountNoSpaces}`;
+
+  const numberCount = analyzer.getNumberCount(event.target.value);
+  document.querySelectorAll(
+    ".item"
+  )[3].textContent = `Recuento de numeros: ${numberCount}`;
+
+  const numberSum = analyzer.getNumberSum(event.target.value);
+  document.querySelectorAll(
+    ".item"
+  )[4].textContent = `Suma total de numeros: ${numberSum}`;
+
+  const averageWordLength = analyzer.getAverageWordLength(event.target.value);
+  document.querySelectorAll(
+    ".item"
+  )[5].textContent = `Longitud media de las palabras: ${averageWordLength.toFixed(
+    2
+  )}`;
+});
+
+button.addEventListener("click", function () {
+  textarea.value = "";
+});
